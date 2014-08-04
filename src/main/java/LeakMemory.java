7ce7ff4eb8b0410dcd6b07a4963d4cc395a9d31a
Kevin Corcoran <kevin.corcoran@puppetlabs.com>
2014-08-04 15:15:38 -0700

    initial commit

---
+++ src/main/java/LeakMemory.java
@@ -0,0 +1,21 @@
+import org.jruby.embed.ScriptingContainer;
+
+public class LeakMemory
+{
+    public static void main( String[] args )
+    {
+        String rubyScript =
+                "Gem.clear_paths"           + '\n' +
+                "begin"                     + '\n' +
+                "  require 'foo'"           + '\n' +
+                "rescue Exception"          + '\n' +
+                "end";
+
+        ScriptingContainer scriptingContainer = new ScriptingContainer();
+
+        System.out.println("Beginning loop...");
+        while (true) {
+            scriptingContainer.runScriptlet(rubyScript);
+        }
+    }
+}
